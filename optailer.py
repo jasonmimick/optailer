7ecac886d9a2ef0cc709cdd814e2b285055a2ce7
Jason Mimick <jmimick@gmail.com>
2016-09-02 14:51:06 -0400

    add stream mode
    adding new mode to allow streaming data from one MongoDB instance to
    another

--- optailer.py
+++ optailer.py
@@ -6,6 +6,8 @@ import sys,time,os
 import signal
 import argparse
 import pymongo
+import bson
+import datetime
 from pymongo import MongoClient, CursorType
 #from bson import TimeStamp
 from threading import Thread
@@ -44,7 +46,7 @@ class App():
 
     def tail(self):
         self.logger.info("optailer tail")
-        namespaces = self.config['namespaces'].split(',')
+        namespaces = self.config['namespaces'].keys()
         self.logger.info("namespaces to tail: " + ", ".join(namespaces))
 
         threads = []
@@ -64,23 +66,43 @@ class App():
         ns = db_name + '.' + coll_name
         local_oplog = 'oplog.' + coll_name
         connection = pymongo.MongoClient(self.config['mongodb'])
-        local_db = connection['local']
-        db = connection[db_name]
-        query = { "ns" : ns }
-
-        if local_oplog in db.collection_names():
-            try:
-                last_local_oplog_entry = db[local_oplog].find({}).sort("ts",-1).limit(1).next()
-                query["ts"]={ "$gt" : last_local_oplog_entry['ts'] }
-            except StopIteration:   #thrown when out of data so wait a little
-                self.vprint(db_name+"."+local_oplog+' exists, but no entries found')
+        mode = self.config['mode']
+        if mode == 'stream':
+            if self.config['sinkMongodb']:
+                sinkConnection = pymongo.MongoClient( self.config['sinkMongodb'] )
+                if self.config['namespaces'][ns]:
+                    sinkNS = self.config['namespaces'][ns]
+                else:
+                    self.logger.error("mode is 'stream' but no mapping for namespace="+ns);
+                    return
+            else:
+                self.logger.error("mode is 'stream' but no 'sinkMongodb' setting found in config file")
+                return
         else:
-            size_bytes = self.config['local_oplog_size_megabytes']*1024*1024
-            self.logger.info(db_name+"."+local_oplog+' not found, attempting to create size='+str(size_bytes)+' bytes')
-            db.create_collection(local_oplog,capped=True,size=size_bytes)
-
-        self.logger.info(query)
+            sinkConnection = connection
+            sinkNS= db_name + "." + local_oplog
+        db = connection[db_name]
+        local_db = connection['local']
+        query = { "ns" : ns }
+        self.logger.info(query);
+        if mode == 'local':
+            self.logger.info("mode=local");
+            if local_oplog in db.collection_names():
+                try:
+                    last_local_oplog_entry = db[local_oplog].find({}).sort("ts",-1).limit(1).next()
+                    query["ts"]={ "$gt" : last_local_oplog_entry['ts'] }
+                except StopIteration:   #thrown when out of data so wait a little
+                    self.vprint(db_name+"."+local_oplog+' exists, but no entries found')
+            else:
+                size_bytes = self.config['local_oplog_size_megabytes']*1024*1024
+                self.logger.info(db_name+"."+local_oplog+' not found, attempting to create size='+str(size_bytes)+' bytes')
+                db.create_collection(local_oplog,capped=True,size=size_bytes)
+        if mode == 'stream':
+            self.logger.info("stream mode")
+            query['ts']={ "$gt" : bson.timestamp.Timestamp(datetime.datetime.now(),0) }
+        self.logger.info("after query set")
         #start tailable cursor
+        self.logger.info(connection)
         oplog = connection['local']['oplog.rs'].find(query,cursor_type = CursorType.TAILABLE_AWAIT)
         if 'ts' in query:
             oplog.add_option(8)     # oplogReplay
@@ -91,26 +113,31 @@ class App():
                     oplog.close()
                     break
                 else:
+                    self.vprint("gonna get next() from oplog")
                     doc = oplog.next()
                     self.vprint(doc)
-		    self.try_insert(db,local_oplog,doc)
+                    self.logger.info("xxx")
+                    self.try_insert(sinkConnection,sinkNS,doc)
             except StopIteration:   #thrown when out of data so wait a little
                 self.vprint("sleep")
                 time.sleep(self.config['tailSleepTimeSeconds'])
 
-    def try_insert(self,db,coll_name, doc):
+    def try_insert(self,connection,ns, doc):
+        db, coll_name = ns.split('.')
         for i in range(5):
-  	    try:
-                wr = db[coll_name].insert_one(doc)
+            try:
+                wr = connection[db][coll_name].insert_one(doc)
                 self.vprint(dir(wr))       # TODO: Check write result!
                 self.vprint("Inserted into " + coll_name)
                 return
             except pymongo.errors.AutoReconnect:
-		self.logger.error("AutoReconnect error, try #" + str(i))
+                self.logger.error("AutoReconnect error, try #" + str(i))
                 time.sleep(pow(2, i))
-	# if here, then we failed 5 times - log fatal error
-	self.logger.critical("Unable to insert document into " + coll_name + " - MongoDB unavailable?") 
-	raise Exception("Unable to insert into MongoDB")
+
+        # if here, then we failed 5 times - log fatal error
+        self.logger.critical("Unable to insert target MongoDB: " + connection)
+        self.logger.critical("Unable to insert document into " + ns + " - MongoDB unavailable?")
+        raise Exception("Unable to insert into MongoDB")
 
     def cleanup(self):
         if self.stop_called:
@@ -121,12 +148,14 @@ class App():
         time.sleep(5)       # sleep to let tailing thread cleanup
         self.logger.info("cleanup complete - optailer shutting down.")
 
+print(sys.argv)
 config_file = sys.argv[2]
 if not os.path.isfile( config_file ):
     print "config file '",config_file," not found"
     sys.exit(1)
 
 config = yaml.safe_load(open( config_file ))
+print config
 logger = logging.getLogger("optailer")
 logger.setLevel(getattr(logging,config.get('loglevel','INFO').upper()))
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
